ff5069dc13775a4d99e1ff8a4910398067d3d00e
ilfan14 <ilfan.highker@gmail.com>
2017-07-17 09:09:28 +0700

    Fix kurang tanda dolar
    dolar oi dolar

--- blog/admin/add_article.php
+++ blog/admin/add_article.php
@@ -29,7 +29,7 @@
     is_uploaded_file($_FILES['foto']['tmp_name']);
     move_uploaded_file($_FILES['foto']['tmp_name'], $dir_upload.$nama_gmb);
 
-    $sql = "INSERT INTO tabel_berita (judul_berita, id_kategori, isi_berita, gbr_berita, penulis, tanggal, jam) VALUES ('judul_berita','kategori_artikel','$isi_artikel','$nama_gmb','$penulis','$tanggal','$jam')";
+    $sql = "INSERT INTO tabel_berita (judul_berita, id_kategori, isi_berita, gbr_berita, penulis, tanggal, jam) VALUES ('$judul_berita','$kategori_artikel','$isi_artikel','$nama_gmb','$penulis','$tanggal','$jam')";
 
     if ($connection->query($sql) === TRUE) {
         header("location:create_article.php");
